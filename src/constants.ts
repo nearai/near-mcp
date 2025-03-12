9053efac640408fb239b423dd35ee2250e167018
Alexander Comerford <alex@taoa.io>
2025-03-12 05:41:05 +0000

    more const and initial cli setup

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const MCP_SERVER_NAME = 'mcp-near';
+export const YOCTO_NEAR_PER_NEAR = 10 ** 24;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from "./services";
-export * from "./types";
+export { run } from '@oclif/core';
+export * from './services';
+export * from './utils';
