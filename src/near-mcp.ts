535b68cdd7f4b29d071a9fa816ebc8762788b178
Alexander Comerford <alex@taoa.io>
2025-04-10 00:46:53 +0000

    move near mcp entrypoint script into src dir

---
+++ src/near-mcp.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { execute } from '@oclif/core';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+async function run() {
+  try {
+    await execute({ dir: __dirname });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+void run();
+
+run().catch((error) => {
+  console.error('Unhandled error:', error);
+  process.exit(1);
+});
